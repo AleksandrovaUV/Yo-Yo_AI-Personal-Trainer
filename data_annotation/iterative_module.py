60f519e492f0f19594186211e0eea7172b7ea386
AleksandrovaUV <nearrmee@gmail.com>
2026-01-19 19:09:45 +0300

    new modules for data annotation

--- data_annotation/iterative_module.py
+++ data_annotation/iterative_module.py
@@ -3,8 +3,10 @@ import numpy as np
 def bone_length(p1, p2):
     return np.linalg.norm(np.array(p1) - np.array(p2))
 
-# пример: плечо → локоть
+
 def check_upper_arm(keypoints):
     left = bone_length(keypoints[11], keypoints[13])
     right = bone_length(keypoints[12], keypoints[14])
     return left, right
+
+
